23008d69ff71b0016d3af0a8e59a11d0f7b73427
wangjiefeng <1414059957@qq.com>
2016-10-02 00:48:11 +0800

    commit

---
+++ problem3._Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,17 @@
+/**
+**参考了lightmark的代码，被他的聪明与才智惊讶到了
+**/
+
+int lengthOfLongestSubstring(string s) 
+{
+    vector<int> dict(256, -1);
+    int maxLen = 0, start = -1;
+    for (int i = 0; i != s.length(); i++)
+    {
+        if (dict[s[i]] > start)
+            start = dict[s[i]];
+        dict[s[i]] = i;
+        maxLen = max(maxLen, i - start);
+    }
+    return maxLen;
+}
